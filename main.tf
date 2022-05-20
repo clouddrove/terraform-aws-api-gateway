2a8e96ee934556ef4afe2405e46bcb9a91d0ea91
Martin Paul Eve <martin@martineve.com>
2022-05-20 10:09:12 +0100

    Pass VPC endpoints conditionally. Fixes #24

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_api_gateway_rest_api" "default" {
 
   endpoint_configuration {
     types            = var.types
-    vpc_endpoint_ids = var.vpc_endpoint_ids
+    vpc_endpoint_ids = length(var.vpc_endpoint_ids) > 0 && var.vpc_endpoint_ids[0] != "" ? var.vpc_endpoint_ids : null
   }
   policy = var.api_policy
   tags   = var.tags
