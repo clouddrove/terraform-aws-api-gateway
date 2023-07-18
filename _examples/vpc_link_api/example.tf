0f72292f3a6084bfa317082396ed6ed8e0dd71f2
theprashantyadav <theprashantyadav7@gmail.com>
2023-07-18 21:30:49 +0530

    feat: update security group version

--- _examples/vpc_link_api/example.tf
+++ _examples/vpc_link_api/example.tf
@@ -44,7 +44,7 @@ module "public_subnets" {
 #tfsec:ignore:aws-ec2-no-public-ingress-sgr
 module "security_group" {
   source  = "clouddrove/security-group/aws"
-  version = "1.3.0"
+  version = "2.0.0"
 
   name          = "security-group"
   environment   = "test"
