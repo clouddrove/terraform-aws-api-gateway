d9dfef71c0169127912eb2bed224c86efdd5da81
theprashantyadav <theprashantyadav7@gmail.com>
2023-10-10 21:08:11 +0530

    feat: update version.tf and remove unuse variable

---
+++ _examples/vpc_link_api/versions.tf
@@ -0,0 +1,11 @@
+# Terraform version
+terraform {
+  required_version = ">= 1.5.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.20.0"
+    }
+  }
+}
