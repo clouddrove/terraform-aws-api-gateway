8a51731e1020897383352724e620283d6258f558
Anmol Nagpal <anmolnagpal@users.noreply.github.com>
2026-03-20 07:14:15 -0400

    ⬆️ upgrade: update AWS provider to >= 5.80.0 and Terraform to >= 1.10.0

--- examples/vpc_link_api/versions.tf
+++ examples/vpc_link_api/versions.tf
@@ -1,11 +1,11 @@
 # Terraform version
 terraform {
-  required_version = ">= 1.6.1"
+  required_version = ">= 5.80.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.20.0"
+      version = ">= 5.80.0"
     }
   }
 }
